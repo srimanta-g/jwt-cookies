608c1ee7c26f3ad4f87958e49861c8edad539cc8
srimanta-g <srimantaganguly02@gmail.com>
2024-04-11 03:14:22 +0530

    Created User Router

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import  Express  from "express";
 import chalk from "chalk";
 import { connectToDatabase } from "./db/connect-db";
+import { userRouter } from "./router/user-router";
 import dotenv from "dotenv";
 
 dotenv.config();
 const app = Express();
+app.use(Express.json());
+app.use(userRouter);
 
 //@ts-ignore
 // connectToDatabase(process.env.MONGODB_URL);
---
+++ src/router/user-router.ts
@@ -0,0 +1,9 @@
+import { Router, Request, Response } from 'express';
+
+const userRouter = Router();
+
+userRouter.post('/user/add-new-user', (req: Request, res:Response) => {
+
+}) ;
+
+export { userRouter };
